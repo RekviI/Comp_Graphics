dd59f0667411fd0d5f154274cc464a38f8789971
Danyil Zemlianyi <danzemlyanoi@gmail.com>
2022-11-10 23:09:39 +0200

    first commit

---
+++ Laboratory 2/Lab_2.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageDraw
+
+img = Image.new('L', (960, 540), 255)
+draw = ImageDraw.Draw(img)
+
+with open("D:\Study\Python files\Comp_Graphics\Laboratory 2\DS6.txt", "r") as file:
+    for line in file:
+        coords_list = line.split()
+        i = int(coords_list[1])
+        j = int(coords_list[0])
+        draw.line((i, j, i + 1, j + 1))
+        
+img.show()
+img.save('result.png')
